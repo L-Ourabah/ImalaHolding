c04b7d8097f3d7987bf6c969175973862471119b
ourabah <ourabah.lou@hotmail.com>
2023-12-06 10:34:49 +0100

    Update index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,20 +3,19 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header.jsx"
-import Footer from"./components/Footer.jsx"
+import Footer from "./components/Footer.jsx"
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-
-  <BrowserRouter>
-    
-    <Header />    
-    <App />
-    <Footer/>
-
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Header />
+      <App />
+      <Footer />
+    </BrowserRouter>
+  </React.StrictMode>
 
 );
