386630e2e80fbdcaa3ae132cdfaa18bb56a6d569
ourabah <ourabah.lou@hotmail.com>
2023-12-31 17:33:30 +0100

    Update index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,21 +1,23 @@
+// index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
-import { BrowserRouter } from "react-router-dom";
-import Header from "./components/Header.jsx"
-import Footer from "./components/Footer.jsx"
 
+// Importation des composants Header et Footer
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
 
-
+// Création du point de rendu
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
   <React.StrictMode>
     <BrowserRouter>
+
       <Header />
       <App />
       <Footer />
     </BrowserRouter>
   </React.StrictMode>
-
 );
+
