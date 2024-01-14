273d8b5a84f5941eb2ccd2d10bdf87790eed2465
ourabah <ourabah.lou@hotmail.com>
2024-01-14 22:00:19 +0100

    Create AirbnbInscription.jsx

---
+++ client/src/pages/Airbnb/AirbnbInscription/AirbnbInscription.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function AirbnbInscription() {
+  return (
+    <div className='inscription'>
+      
+      
+      AirbnbInscription
+      
+      
+      
+      </div>
+  )
+}
