fbbdb76274397adb35f9eea883c65f2109e5e1e7
ourabah <ourabah.lou@hotmail.com>
2024-01-15 02:47:48 +0100

    Update AirbnbInscription.jsx

--- client/src/pages/Airbnb/AirbnbInscription/AirbnbInscription.jsx
+++ client/src/pages/Airbnb/AirbnbInscription/AirbnbInscription.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+
 import Header from '../../../components/Header/HeaderConexInscri.jsx'
+import Form from './Formulaire/FormAirbnbInscription.jsx'
 
 import './AirbnbInscription.css'
 export default function AirbnbInscription() {
@@ -9,30 +10,7 @@ export default function AirbnbInscription() {
 
 
       <Header />
-
-      <div className="title-inscription">
-        <h2>
-          INSCRIPTION
-        </h2>
-      </div>
-      <div className="formulaire">
-
-        <div className="nomPrenom">
-          <div className="prenom"></div>
-          <div className="nom"></div>
-
-        </div>
-
-        <div className="dateNs"></div>
-        <div className="email"></div>
-        <div className="mpd"></div>
-        <div className="btnInscription">
-          <button>S'inscrire</button>
-          <p>Vous êtes déjà membre ?</p>
-          <p><Link to='/connexion'>Connectez-vous</Link></p>
-        </div>
-      </div>
-
+      <Form/>
     </div>
   )
 }
