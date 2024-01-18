a3946cf2ed1d8e8e3d6121e6374450c602e5c928
Stella <90323575+stellaric@users.noreply.github.com>
2024-01-18 12:48:57 +0100

    Ajout Page Accueil

---
+++ client/src/pages/Airbnb/AirbnbAccueil/components/HeaderAccueil.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from '../../componentsG/Header'
+
+export default function HeaderAccueil() {
+  return (
+    <header className="header_airbnb_accueil">
+    <Navbar/>
+      <h1> Découvrez nos nombreux <br/> appartement à louer  </h1>
+      </header>
+  )
+}
