a3caf67a9a3bba15c5ab9e80c088292909f4ef1f
ourabah <ourabah.lou@hotmail.com>
2024-01-24 12:12:57 +0100

    Update Realisations.jsx

--- client/src/pages/Construction/Realisation/Realisations.jsx
+++ client/src/pages/Construction/Realisation/Realisations.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import './Realisations.css'
 import Herobanner from './Herobanner/Herobanner'
 import Main from './Main/Main'
+import Navbar from '../../../components/NavBars/NavbarConstruction'
 
 function Realisations() {
   return (
     <div className='realisations-container'>
+         <Navbar/>
         <div className='realisations-container'>
             <Herobanner />
             <Main />
