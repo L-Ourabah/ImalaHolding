283d9c30e7f8b872baf99a674af6354cd678e56a
ourabah <ourabah.lou@hotmail.com>
2024-01-24 12:12:42 +0100

    Update Conception.jsx

--- client/src/pages/Construction/Conception/Conception.jsx
+++ client/src/pages/Construction/Conception/Conception.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import './Conception.css'
 import Herobanner from './Herobanner/Herobanner'
 import Pannels from './Pannels/Pannels'
+import Navbar from '../../../components/NavBars/NavbarConstruction'
 
 function Conception() {
   return (
     <div className='conception-container'>
+      <Navbar/>
       <div className='conception-wrapper'>
         <Herobanner />
         <Pannels />
