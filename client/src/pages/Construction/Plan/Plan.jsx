f9a665dc3bf8adaa035dc7b9086e42f25874e13a
ourabah <ourabah.lou@hotmail.com>
2024-01-24 12:12:55 +0100

    Update Plan.jsx

--- client/src/pages/Construction/Plan/Plan.jsx
+++ client/src/pages/Construction/Plan/Plan.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import './Plan.css'
 import Herobanner from './Herobanner/Herobanner'
 import Pannels from './Pannels/Pannels'
+import Navbar from '../../../components/NavBars/NavbarConstruction'
 
 function Plan() {
   return (
     <div className='plan-container'>
+      <Navbar/>
         <div className='plan-container'>
             <Herobanner />
             <Pannels />
