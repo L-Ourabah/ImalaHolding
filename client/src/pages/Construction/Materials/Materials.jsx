6123697f7de1f6a83c6174778f7de94a3f38c548
ourabah <ourabah.lou@hotmail.com>
2024-01-24 12:12:52 +0100

    Update Materials.jsx

--- client/src/pages/Construction/Materials/Materials.jsx
+++ client/src/pages/Construction/Materials/Materials.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import './Materials.css'
 import Herobanner from './Herobanner/Herobanner'
 import Pannels from './Pannels/Pannels'
+import Navbar from '../../../components/NavBars/NavbarConstruction'
 
 function Materials() {
   return (
     <div className='materials-container'>
+      <Navbar/>
         <div className='materials-container'>
             <Herobanner />
             <Pannels />
