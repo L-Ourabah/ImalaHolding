1edd9c8e09c9773e7a0cf74233cc44dc6fa62b24
AaronSaada <aaron.saada0@gmail.com>
2024-02-07 18:32:22 +0100

    css

--- client/src/pages/Construction/Home/Herobanner/Herobanner.jsx
+++ client/src/pages/Construction/Home/Herobanner/Herobanner.jsx
@@ -8,12 +8,11 @@ function Herobanner() {
         </div>
         <div className='home-herobanner-message'>
             <div className='container'>
-                <h2>Bienvenue sur notre plateforme immobilière <br /> IMALA CONSTRUCTION.</h2>
+                <h2>Bienvenue sur<br /> IMALA CONSTRUCTION</h2>
                 <p>
                   Nous sommes passionnés par la transformation de vos rêves en réalité. Que vous cherchiez à
                   construire votre villa, un immeuble ou à développer un projet immobilier, nous sommes là pour vous
-                  guider à chaque étape de votre projet, de la conception à la livraison finale. Explorez nos offres et
-                  découvrez comment nous pouvons vous aider à réaliser votre vision.
+                  guider à chaque étape de votre projet, de la conception à la livraison finale.
                 </p>
                 <button>Voir plus</button>
             </div>
--- client/src/pages/Construction/Home/ServicesSection/ServicesSection.jsx
+++ client/src/pages/Construction/Home/ServicesSection/ServicesSection.jsx
@@ -5,12 +5,10 @@ import './ServicesSection.css'
 function ServicesSection() {
   return (
     <div className='home-services-section'>
-        <h2>Découvrez les services exceptionnels que nous offrons avec <br /> IMALA Construction.</h2>
+        <h2>Découvrez nos services exceptionnels</h2>
         <p>
             Que vous soyez un particulier ou un promoteur immobilier, nous vous aidons à réaliser vos projets
-            immobiliers. Que ce soit pour un plan, la conception, la construction, la rénovation ou la gestion de
-            votre bien immobilier, notre équipe dédiée est là pour vous accompagner à chaque étape de votre
-            projet.
+            immobiliers.
         </p>
         <div className='services-cards'>
             <Link to='/plan'>
